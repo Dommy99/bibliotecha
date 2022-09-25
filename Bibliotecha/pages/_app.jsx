c443bd088495b48dd3a5ec03eea270adc242e042
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2022-09-24 22:13:18 -0500

    file set up

---
+++ Bibliotecha/pages/_app.jsx
@@ -0,0 +1,40 @@
+import '../styles/globals.css'
+import Header from "../components/header"
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+
+const App = () => {
+  return(
+    <div>
+      {/* <Router> */}
+      {/* <Header/> */}
+      {/* <Intro/>
+      <Routes>
+      <Route 
+                path="/Portfolio"
+                element={<ProductList />}
+              />
+      <Route 
+                path="/About"
+                element={<About />}
+              />
+      <Route 
+                path="/Contact"
+                element={<Contact />}
+              />
+      <Route 
+                path="/Resume"
+                element={<Resume/>}
+              />
+      </Routes> */}
+      
+      {/* </Router> */}
+      {/* <Footer/> */}
+      
+      </div>
+  ) 
+};
+
+
+export default App;
---
+++ Bibliotecha/pages/index.jsx
@@ -0,0 +1,66 @@
+// import Head from "next/head";
+// import RegisterForm from "components/RegisterForm";
+// import LoginForm from "components/LoginForm";
+// import fetchTokenData from "utils/fetchTokenData";
+// import { useDispatch } from "react-redux";
+
+const Home = () => {
+
+  return (
+    <>
+      {/* <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head> */}
+
+      <div id="lp-register">
+        <div className="container wrapper">
+          <div className="row">
+            <div className="col-sm-5">
+              <div className="intro-texts">
+                <h1 className="text-white">Add headline here !!!!</h1>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+                <button className="btn btn-primary">Learn More</button>
+              </div>
+            </div>
+
+            <div className="col-sm-6 col-sm-offset-1">
+              <div className="reg-form-container">
+                <div className="reg-options">
+                  <ul className="nav nav-tabs">
+                    <li>
+                      <a href="#register" data-toggle="tab">
+                        Register
+                      </a>
+                    </li>
+                    <li className="active">
+                      <a href="#login" data-toggle="tab">
+                        Login
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+// Home.getInitialProps = async ({ req }) => {
+//   const res = await (await fetchTokenData(req)).json();
+
+//   return {
+//     userId: res?.userId || null,
+//   };
+// };
+
+export default Home;

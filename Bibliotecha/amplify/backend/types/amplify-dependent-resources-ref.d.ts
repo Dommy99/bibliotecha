e8b6aa650ea66babd1ed47771507ad09df1dd46e
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2022-10-13 22:49:07 -0500

    aws auth

--- Bibliotecha/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ Bibliotecha/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,23 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "bibliotecha": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "bibliotechac0986ffd": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "BibliotechaGroupRole": "string"
+        }
+    }
+}
--- Bibliotecha/pages/_app.jsx
+++ Bibliotecha/pages/_app.jsx
@@ -1,12 +1,21 @@
 import '../styles/globals.css'
 import Layout from '../components/layout'
 
-function MyApp({ Component, pageProps }) {
+// amplify 
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from '../src/aws-exports';
+Amplify.configure(awsExports);
+
+function App({ Component, pageProps,signOut, user }) {
   return (
   <Layout>
   <Component {...pageProps} />
+  <Heading level={1}>Hello {user.username}</Heading>
+      <Button onClick={signOut}>Sign out</Button>
   </Layout>
   )
 }
 
-export default MyApp
+export default withAuthenticator(App);

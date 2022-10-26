baed09ea06ed13669e4d0ed1dc081ca21888c2f7
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2022-10-26 18:51:10 -0500

    aws styles

--- Bibliotecha/pages/_app.jsx
+++ Bibliotecha/pages/_app.jsx
@@ -3,19 +3,27 @@ import Layout from '../components/layout'
 
 // amplify 
 import { Amplify } from 'aws-amplify';
-import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+// import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import { Authenticator} from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from '../src/aws-exports';
 Amplify.configure(awsExports);
+const component = {Header(){return <h1>hello</h1>}}
 
-function App({ Component, pageProps,signOut, user }) {
+export default function App({ Component, pageProps,signOut, user }) {
   return (
+    <Authenticator components ={component}> 
+    {({ signOut, user}) => (
   <Layout>
-  <Component {...pageProps} />
-  <Heading level={1}>Hello {user.username}</Heading>
-      <Button onClick={signOut}>Sign out</Button>
+  
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      
   </Layout>
+  )}
+  </Authenticator>
   )
 }
 
-export default withAuthenticator(App);
